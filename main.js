256a49e566962a339d4d0bbfa138be04e926881f
Alexandra <127262337+pcalexandrat@users.noreply.github.com>
2024-06-20 08:34:59 -0300

    main

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
+
 for(let i=0; i<botoes.length; i++){
     botoes[i].onclick = function(){
         for(let j=0; j<botoes.length; j++){
@@ -18,11 +19,6 @@ const tempoObjetivo4 = new Date("2024-12-25T00:00:00");
 
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
-contadores[0].textContent = calculaTempo(tempoObjetivo1);
-for (let i=0; i < contadores.length; i++){
-    contadores[i].textContent = calculaTempo(tempos[i]);  
-}
-
 function calculaTempo(tempoObjetivo) {
     let tempoAtual = new Date();
     let tempoFinal = tempoObjetivo - tempoAtual;
@@ -30,14 +26,17 @@ function calculaTempo(tempoObjetivo) {
     let minutos = Math.floor(segundos / 60);
     let horas = Math.floor(minutos / 60);
     let dias = Math.floor(horas / 24);
+
     segundos %= 60;
     minutos %= 60;
-    horas %= 24; if (tempoFinal > 0){
+    horas %= 24;
+    if (tempoFinal > 0){
         return [dias,horas,minutos,segundos];
     } else {
         return [0,0,0,0];
     }
 }
+
 function atualizaCronometro(){
     for (let i=0; i<contadores.length;i++){
         document.getElementById("dias"+i).textContent = calculaTempo(tempos[i])[0];
