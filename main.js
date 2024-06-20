5599cbea27360db972798f793c81432aef0a748f
Alexandra <127262337+pcalexandrat@users.noreply.github.com>
2024-06-20 08:24:02 -0300

    atualiza cronômetro

--- main.js
+++ main.js
@@ -35,3 +35,18 @@ function calculaTempo(tempoObjetivo) {
     horas %= 24; 
     return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
 }
+function atualizaCronometro(){
+    for (let i=0; i<contadores.length;i++){
+        document.getElementById("dias"+i).textContent = calculaTempo(tempos[i])[0];
+        document.getElementById("horas"+i).textContent = calculaTempo(tempos[i])[1];
+        document.getElementById("min"+i).textContent = calculaTempo(tempos[i])[2];
+        document.getElementById("seg"+i).textContent = calculaTempo(tempos[i])[3];   
+    }
+}
+
+function comecaCronometro(){
+    atualizaCronometro();
+    setInterval(atualizaCronometro,1000);
+}
+
+comecaCronometro();
