267ac4598bfe2e286f5da0fa34cfb60df8b240a4
Alexandra <127262337+pcalexandrat@users.noreply.github.com>
2024-07-04 22:38:08 -0300

    data

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ for(let i=0; i<botoes.length; i++){
 }
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2024-07-05T00:00:00");
-const tempoObjetivo2 = new Date("2024-06-26T00:00:00");
+const tempoObjetivo2 = new Date("2024-07-08T00:00:00");
 const tempoObjetivo3 = new Date("2024-12-14T00:00:00");
 const tempoObjetivo4 = new Date("2024-12-25T00:00:00");
 
